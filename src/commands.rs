ae9206c8d42d13bf69c83c441702d07b48572060
Aidan M <aidan@muttleyville.org>
2023-12-12 22:01:49 +0000

    bosh

--- src/commands.rs
+++ src/commands.rs
@@ -3,8 +3,6 @@ use std::io::{stdin, stdout, Write};
 use std::path::PathBuf;
 use std::fs;
 
-use crate::premake5::PremakeConfig;
-
 pub fn new(name: Option<String>) -> anyhow::Result<()> {
 	let name = if let Some(name) = name {
 		name
@@ -22,20 +20,6 @@ pub fn new(name: Option<String>) -> anyhow::Result<()> {
 
 	fs::create_dir(project_dir.clone()).context("Failed to create project directory")?;
 	fs::create_dir(project_craft_dir.clone()).context("Failed to create `.craft` directory in project folder")?;
-
-	let mut premake_file = project_craft_dir.clone();
-	premake_file.push("premake5");
-	premake_file.set_extension("lua");
-
-	let premake5config = PremakeConfig::new()
-		.set_project_name(name)
-		.set_language("C++".to_string())
-		.add_include_dir(PathBuf::from("include"))
-		.add_include_dir(PathBuf::from("include2"));
-	
-	premake5config.write_to_file(&premake_file)?;
-
-	println!("{}", premake5config);
 
 	return Ok(());
 }
